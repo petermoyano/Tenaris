1aa9ef8b211a8f1a285ebf5d376f9c7d4a4432e6
Peter <pedromoyano454@gmail.com>
2023-09-26 17:25:24 -0300

    Add collection logic and basic qa chain

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
-from dotenv import load_dotenv
+
+import streamlit as st
+from PyPDF2 import PdfReader
 import os
 
 def main():
-    print(os.getenv("OPENAI_API_KEY"))
 
+    st.set_page_config(page_title="Chat with your files", page_icon="🧠")
+    st.header("Chat with your files")
+
+    # upload file
+    pdf = st.file_uploader("Upload your file.", type=None, accept_multiple_files=True, key="uploader")
+
+
+    #extract the text from the pdf
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+        st.write(text)
 
 
 if __name__=='__main__':
